4733277cce098e3161a92265290fb8976a4e24ce
Kldiashvili12 <nikakldiashvili12@gmail.com>
2023-05-20 16:04:21 +0400

    new

---
+++ js.js
@@ -0,0 +1,15 @@
+$('.nav-1').hoover(function(){
+    $('.nav-items-1').toogleClass('navActive')
+    })
+    
+    $('.nav-2').hoover(function(){
+    $('.nav-items-2').toogleClass('navActive')
+    })
+    
+    $('.nav-3').hoover(function(){
+    $('.nav-items-3').toogleClass('navActive')
+    })
+    
+    $('.nav-4').hoover(function(){
+    $('.nav-items-4').toogleClass('navActive')
+    })
